8c82689ce90ec80eef43d6d6e946ebbb8747b63b
Sergio Queiroz <srmq@cin.ufpe.br>
2020-02-07 14:59:02 -0300

    added command to create db tables

---
+++ hello_app/dbfuncs.py
@@ -0,0 +1,15 @@
+from .dbclasses import Base
+from sqlalchemy import create_engine
+import os
+
+def _get_db_URL():
+    db_URL = os.environ.get('DATABASE_URL', '')
+    return db_URL
+
+def create_tables():
+    db_URL = _get_db_URL()
+    if not db_URL:
+        raise Exception('DATABASE_URL environment variable is not defined')
+
+    engine = create_engine(db_URL, echo=True)
+    Base.metadata.create_all(engine)
