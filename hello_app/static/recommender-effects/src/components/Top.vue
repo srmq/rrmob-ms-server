10a9515fd9378ba82db6f4853864d8cc914fd12d
Sergio Queiroz <srmq@srmq.org>
2020-03-03 15:48:49 -0300

    workinprogress

--- hello_app/static/recommender-effects/src/components/Top.vue
+++ hello_app/static/recommender-effects/src/components/Top.vue
@@ -6,12 +6,27 @@
             Bem-vindo(a) ao Recommender Effects!
           </h1>
 
-          <div v-if="!isEmailVerified()">
+          <div v-if="errored">
+            <p  class="subheading font-weight-regular">
+              Houve um erro de carregamento do sistema. Por favor, tente novamente mais tarde.
+            </p>
+          </div>
+          <div v-else-if="loading">
+            <p  class="subheading font-weight-regular">
+              Carregando...
+            </p>
+          </div>
+          <div v-else-if="!isEmailVerified">
             <p  class="subheading font-weight-regular">
               Foi-lhe enviada uma mensagem de verificação para seu endereço de e-mail
-              informado. Por favor, clique no link da mensagem para confirmar o seu 
-              endereço. Caso não receba a mensagem, clique neste link FIXME para enviarmos 
-              novamente.
+               informado. Por favor, clique no link da mensagem para confirmar o seu
+               endereço. Caso não receba a mensagem, clique neste link FIXME para enviarmos
+               novamente.
+            </p>
+          </div>
+          <div v-else>
+            <p  class="subheading font-weight-regular">
+              Email verificado.
             </p>
           </div>
         </v-col>
@@ -28,25 +43,26 @@ export default {
   props: ['loginInfo'],
 
   data: () => ({
-    // TODO
+    loading: true,
+    errored: false,
+    isEmailVerified: null
   }),
 
   created() {
     axios.defaults.headers.common = {'Authorization': `Bearer ${this.loginInfo.access_token}`};
   },
 
-  methods: {
-    async isEmailVerified() {
-      const result = await axios.get('/isemailverified')
-        .then(function (response) {
-          return response.data.result;
-        })
-        .catch(function (error) {
-          console.log(error);
-          return false;
-        });
-      return result;
-    }
-  }    
+  mounted() {
+    axios
+      .get('/isemailverified')
+      .then(response => {
+        this.isEmailVerified = response.data.result;
+      })
+      .catch(error => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => this.loading = false)    
+  }
 }
 </script>
