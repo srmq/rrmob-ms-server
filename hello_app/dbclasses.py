762345ce168b3bf42bbcfb8411b6b691bc200944
srmq <srmq@cin.ufpe.br>
2020-03-25 16:18:34 -0300

    workinprogress

--- hello_app/dbclasses.py
+++ hello_app/dbclasses.py
@@ -16,7 +16,7 @@ class User(Base):
     email_verified = Column(Boolean, default = False, server_default="FALSE")
     verify_code = Column(String(32))
     invitee_id = Column(Integer, ForeignKey('invitees.id'), nullable=False, unique=True)
-    registered_usr = relationship("Invitee", back_populates="registered_usr")    
+    invitation = relationship("Invitee", back_populates="registered_usr")    
     pass_hash = Column(String(128), nullable=False)
     pass_salt = Column(String(32), nullable=False)
     spot_id = Column(String(1024))
@@ -30,7 +30,7 @@ class Invitee(Base):
     __tablename__ = 'invitees'
     id = Column('id', Integer, primary_key=True)
     email = Column(String(320), nullable=False, unique=True, index=True)
-    registered_usr = relationship("User", back_populates="invite", uselist=False)
+    registered_usr = relationship("User", back_populates="invitation", uselist=False)
 
     def __repr__(self):
         return "{\"id\": %s, \"email\" = %s}" % (self.id, self.email)
